e696fa7a62d1175eb43d08699b83d4442ca9fbea
grafitek <jozeflenarcic@gmail.com>
2020-11-23 19:12:03 +0100

    upload code.php

---
+++ code.php
@@ -0,0 +1,24 @@
+<!-- Floating Facebook Like Box Widget -->
+<script type = "text/javascript" src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type = "text/javascript" language = "javascript">$(document).ready(function() {$('#scroll').hover(function () {$(this).animate({"right":"0px"},350);},function () {$(this).animate({"right":"-344px"},350);});});</script>
+
+<style>#scroll{float:right; position:fixed; font-family: sans-serif; top:35%; right:-344px; z-index:99999;
+}
+.fb-text{float: left; background: #2851A3; background: url('https://2.bp.blogspot.com/-3q-O1L1gEWk/V4XZ48M8QMI/AAAAAAAAAbc/J4_TV4O5B64wg10Vac7712xRB63gqPOegCKgB/s1600/fb_widget.png') no-repeat scroll left center transparent !important; z-index: 99999; height: 200px; right:-100px; width: 48px;}
+.fb-frame{float: left; width: 340px; height:214px; background:lightgray; border: 2px solid #2851A3;}
+.fb-frame div{padding:0;}
+.fb-att{float: right;}
+.fb-att a{text-decoration: none; color: white; font-size: 10px; background: #2851A3; padding: 3px; display: block;}
+.fb-att a:hover{text-decoration:underline; color:white;}</style>
+
+<div id='scroll'>
+<!--FFLBW by www.grafitek.sk -->
+<div class="fb-text"></div>
+<div class="fb-frame">
+<div><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FSoftNabytok&tabs&width=340&height=130&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId=223174605226307" width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
+</div></div>
+<div style="clear:both;"></div>
+<div class="fb-att"><a href="https://grafitek.sk" target="_blank">by Grafitek.sk</a></div>
+<!--FFLBW by www.grafitek.sk -->
+</div>
+<!-- Floating Facebook Like Box Widget -->
